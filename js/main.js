6edecfa3ec7c68a6da24c8ffad9d7daddbc7380a
John van der Loo <jvanderloo@gmail.com>
2020-07-28 01:44:44 +1000

    Remove the conditional check for window.crypto, only browsers with it are supported .

--- js/main.js
+++ js/main.js
@@ -188,31 +188,22 @@ function CorrectHorseBatteryStaple() {
 	 * the crypto API. It will be a number in the range starting
 	 * with lower and up to (but without) upper.
 	 *
-	 * @param {number} lower The lower bound of the range of numbers.
-	 * @param {number} upper The upper bound of the range of numbers (not included).
+	 * @param {number} lower - The lower bound of the range of numbers.
+	 * @param {number} upper - The upper bound of the range of numbers (not included).
 	 */
 	this.getUniformRandomInteger = function(lower, upper) {
 		lower = Math.round(lower);
 		upper = Math.round(upper);
 
-		var difference = upper - lower;
+		const difference = upper - lower;
 
 		if (difference <= 0) {
 			throw Error("The upper bound is not greater than the lower bound.");
 		}
 
-		// get a cryptoObject to access the Crypto-API
-		var cryptoObject = window.crypto || window.msCrypto;
-
-		if ((!cryptoObject) || (typeof cryptoObject.getRandomValues !== "function") || (!Uint32Array)) {
-			var message = "Your browser has no secure source of random numbers.";
-			alert(message)
-			throw Error(message);
-		}
-
-		var maxUint32 = Math.pow(2, 32) - 1;
-		var maxRandomNumber = Math.floor(maxUint32 / difference) * difference;
-		var randomArray = new Uint32Array(1);
+		const maxUint32 = Math.pow(2, 32) - 1;
+		const maxRandomNumber = Math.floor(maxUint32 / difference) * difference;
+		const randomArray = new Uint32Array(1);
 
 		/*
 		 * Generate random numbers until they are smaller than
@@ -221,7 +212,7 @@ function CorrectHorseBatteryStaple() {
 		 * operator will introduce a bias.
 		 */
 		do {
-			cryptoObject.getRandomValues(randomArray);
+			window.crypto.getRandomValues(randomArray);
 		} while (randomArray[0] > maxRandomNumber);
 
 		return lower + (randomArray[0] % difference);
